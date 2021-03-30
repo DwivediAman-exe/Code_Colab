1b48df7a63a12586578002f8838321b7dca8dc6e
Aman Dwivedi <19dwivediaman@gmail.com>
2021-03-30 13:49:52 +0530

    about

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Codecolab - platform for sharing code' });
+  res.render('index', { title: 'CodeColab - platform for sharing code' });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Codecolab - platform for sharing code' });
+});
+
+router.route('/contact')
+.get( function(req, res, next) {
+  res.render('contact', { title: 'Codecolab - platform for sharing code' });
+})
+.post( function(req, res, next) {
+	res.render('thank', { title: 'Codecolab - platform for sharing code' });
 });
 
 module.exports = router;
