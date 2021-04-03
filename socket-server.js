3a0532b0bd8b98df2df08e3bb0686406795b372f
Aman Dwivedi <19dwivediaman@gmail.com>
2021-04-03 21:13:00 +0530

    scoket real time bug fix

--- socket-server.js
+++ socket-server.js
@@ -1,22 +1,40 @@
 'use strict';
 
 var socketIO = require('socket.io');
+var ot = require('ot');
+var roomList = {};
 
 module.exports = function(server) {
-	var io = socketIO(server);
-	io.on('connection', function(socket) {
-		socket.on('joinRoom', function(data) {
-			socket.room = data.room;
-			socket.join(data.room);
-		});
+  var str = 'This is a Markdown heading \n\n' +
+            'var i = i + 1;';
 
-		socket.on('chatMessage', function(data) {
-			io.to(socket.room).emit('chatMessage', data);
-		})
+  var io = socketIO(server);
+  io.on('connection', function(socket) {
+    socket.on('joinRoom', function(data) {
+      if (!roomList[data.room]) {
+        var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb) {
+          var self = this;
+          Task.findByIdAndUpdate(data.room, {content: self.document}, function(err) {
+            if (err) return cb(false);
+            cb(true);
+          });
 
-		socket.on('disconnet', function(){
-			socket.leave(scoket.room);
-		});
+        });
+        roomList[data.room] = socketIOServer;
+      }
+      roomList[data.room].addClient(socket);
+      roomList[data.room].setName(socket, data.username);
 
-	})
-};
+      socket.room = data.room;
+      socket.join(data.room);
+    });
+
+    socket.on('chatMessage', function(data) {
+      io.to(socket.room).emit('chatMessage', data);
+    });
+
+    socket.on('disconnect', function() {
+      socket.leave(socket.room);
+    });
+  })
+}
